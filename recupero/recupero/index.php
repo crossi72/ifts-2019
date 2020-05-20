4f96ee9f1e4f6c6ef61d366dd5b702b70dc6429a
crossi72 <claudio@eurosoftlab.com>
2020-05-20 12:26:21 +0200

    added "recupero" folder

---
+++ recupero/recupero/index.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+	<link rel="stylesheet" href="style.css" type="text/css" media="all">
+</head>
+<body>
+	<div>
+		<?php
+			stampa('primo titolo', 'titolo-importante', 'H');
+			stampa('testo di prova', 'blu grande', 'P');
+			stampa_titolo('secondo titolo', 'titolo-grande');
+			stampa_testo('testo di prova', array('citazione','grande'), 2);
+		?>
+	</div>
+	<div>
+		<?php
+			stampa_titolo('lezione', 'titolo-importante');
+			stampa_testo('testo di prova', array('blu', 'grande'), 1);
+			stampa_titolo('lezione', 'titolo-grande');
+			stampa_testo('testo di prova', array('blu'), 5);
+		?>
+	</div>
+	<div>
+		<img src="immagine.jpg" class="piccola">
+	</div>
+</body>
+</html>
+
+<?php
+
+	/**
+	 * Stampa un titolo HTML
+	 *
+	 * @param [type] $testo: testo da inserire nel titolo
+	 * @param [type] $classe: classe css da assegnare al tag H1
+	 * @return void
+	 */
+	function stampa_titolo($testo, $classe){
+		echo '<h1 class="' . $classe . '">';
+		echo $testo;
+		echo '</h1>';
+	}
+
+	/**
+	 * Stampa un testo generico
+	 *
+	 * @param [type] $testo: contenuto del tag da stampare
+	 * @param [type] $classe: classe css da applicare al tag contenitore
+	 * @param [type] $tipo: accetta H o P
+	 * @return void
+	 */
+	function stampa($testo, $classe, $tipo){
+		if ($tipo == 'P'){
+			//stampo un paragrafo
+			echo '<p class="' . $classe . '">';
+			echo $testo;
+			echo '</p>';
+		} else {
+			//stampo un titolo
+			echo '<h1 class="' . $classe . '">';
+			echo $testo;
+			echo '</h1>';
+		}
+	}
+
+	/**
+	 * Stampa un paragrafo HTML
+	 *
+	 * @param [type] $testo: testo da inserire nel paragrafo
+	 * @param [type] $classe: classe css da assegnare al tag <p>
+	 * @param [type] $ripetizioni: quante volte deve essere stampato il testo
+	 * @return void
+	 */
+	function stampa_testo($testo, $classe, $ripetizioni){
+		for ($i = 0; $i < $ripetizioni; $i++){
+			echo '<p class="' . estrai_classi($classe) . '">';
+			echo $testo;
+			echo '</p>';
+		}
+	}
+
+	/**
+	 * Trasforma un array di stringhe in una stringa separata con ' ' (spazio)
+	 *
+	 * @param [type] $classi: array contenente le classi
+	 * @return stringa contenente le classi separate da uno spazio
+	 */
+	function estrai_classi($classi){
+		$risultato = '';
+
+		for ($i = 0; $i < count($classi); $i++){
+			$risultato = $risultato . $classi[$i] . ' ';
+		}
+
+		return $risultato;
+	}
+
+?>
